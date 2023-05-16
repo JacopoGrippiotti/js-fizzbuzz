6b108ad2d39b694cd3cfdeb68f4b633b6857bd59
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-16 18:07:22 +0200

    added style classes to html with script.js file to complete BONUS 2

--- js/script.js
+++ js/script.js
@@ -5,25 +5,24 @@ for (let i = 1; i < 101; i++){
     
     if (i % 3 === 0 && i % 5 === 0){
         liElement.append("FizzBuzz")
-        fizzBuzzList.append(liElement)
+        liElement.classList.add("fizzbuzz")
         console.log("FIzzBuzz")
     }
     else if (i % 3 === 0){
         liElement.append("Fizz")
-        fizzBuzzList.append(liElement)
+        liElement.classList.add("fizz")
         console.log("Fizz")
     } 
     else if (i % 5 === 0){
-        
         liElement.append("Buzz")
-        fizzBuzzList.append(liElement)
+        liElement.classList.add("buzz")
         console.log("Buzz")
     }
-
     else{
         liElement.append(i)
-        fizzBuzzList.append(liElement)
+        liElement.classList.add("number")
         console.log(i)
     }
 
+    fizzBuzzList.append(liElement)
 }
