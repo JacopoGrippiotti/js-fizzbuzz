c4cff0bc48595f0c132bcf3563ef618595b994b1
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-16 17:12:51 +0200

    structuring script.js file for BONUS 1

--- js/script.js
+++ js/script.js
@@ -1,19 +1,33 @@
+const fizzBuzzList = document.getElementsByClassName("fizzBuzzList")
+
 for (let i = 1; i < 101; i++){
- 
+    let liElement = document.createElement('li')
     if (i % 3 === 0 && i % 5 === 0){
+        
+        liElement.append("FizzBuzz")
+        fizzBuzzList.append(liElement)
         console.log("FIzzBuzz")
     }
     else if (i % 3 === 0){
+        
+        liElement.append("Fizz")
+        fizzBuzzList.append(liElement)
         
         console.log("Fizz")
     
     } 
     else if (i % 5 === 0){
         
+        liElement.append("Buzz")
+        fizzBuzzList.append(liElement)
         console.log("Buzz")
     }
 
     else{
+        
+        liElement.append(i)
+        fizzBuzzList.append(liElement)
         console.log(i)
     }
+
 }
